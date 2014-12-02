4cce7b5e1ab2bef3b8e9a052f4102c21de8d24ec
codesliced <codesliced@gmail.com>
2014-12-01 16:08:21 -0800

    added create and destroy omniauth authentication methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,13 @@
 class SessionsController < ApplicationController
   def create
-    raise request.env["omniauth.auth"].to_yaml
+    auth = request.env["omniauth.auth"].to_yaml
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    redirect_to :landing_page, :notice => "Signed in"
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to :landing_page, :notice => "Signed out"
   end
 end
