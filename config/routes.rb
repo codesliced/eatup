5d9f6858d519cc392b6cfe046637cd927830f891
codesliced <codesliced@gmail.com>
2014-12-01 16:09:04 -0800

    added route for signing out of meetup.com

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get '/', to: 'groups#index', as: :landing_page
   
   match "/auth/:provider/callback", to: "sessions#create", via: [:get, :post]
+  match "/signout" => "sessions#destroy", :as => :signout, via: [:get, :post]
 end
